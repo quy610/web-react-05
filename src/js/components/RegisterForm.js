00d7414ed8e81c208bb22d9117c7da1367cd8975
quy610 <dinhducquy610@gmail.com>
2020-05-15 17:57:23 +0700

    add validattion

--- src/js/components/RegisterForm.js
+++ src/js/components/RegisterForm.js
@@ -15,84 +15,117 @@ const RegisterForm = (props) => {
   const [notifi, setNotifi] = useState('');
   const [modalShow, setModalShow] = useState(false);
   const [title, setTitle] = useState('');
-  const [error, setError] = useState('');
+  const [error, setError] = useState({ email: '', name: '', password: '' });
 
+  const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const infoUser = { email, name, password };
-    // validateForm(infoUser);
-    // if (error) return;
-
-    axios({
-      method: 'post',
-      url: 'https://server-restful-api-0610.herokuapp.com/api/user/register',
-      data: infoUser
-    })
-      .then(res => {
-        setTitle('Success');
-        setModalShow(true);
-        setNotifi('You create an account successfully');
+  const handleSubmit = () => {
+    if (validateForm()) {
+      const infoUser = { email: email, name: name, password: password };
+      axios({
+        method: 'post',
+        url: 'https://server-restful-api-0610.herokuapp.com/api/user/register',
+        data: infoUser
       })
-      .catch(err => {
-        setTitle('Fail');
-        setModalShow(true);
-        setNotifi(err.response.data);
-      })
+        .then(res => {
+          setTitle('Success');
+          setModalShow(true);
+          setNotifi('You create an account successfully');
+        })
+        .catch(err => {
+          setTitle('Fail');
+          setModalShow(true);
+          setNotifi(err.response.data);
+        })
+    } else {
+      console.log('Invalid form');
+    }
   }
 
-  const validateForm = (user) => {
-    if (user.email === '') {
-      setError('No email');
+  const validateForm = () => {
+    if (validEmailRegex.test(email)) {
+      setError(preState => {
+        return { ...preState, email: '' };
+      });
+    } else {
+      setError(preState => {
+        return { ...preState, email: 'Email is not valid!' };
+      });
     }
 
-    if (user.name === '') {
-      setName('No name');
+    if (name.length < 5) {
+      setError(preState => {
+        return { ...preState, name: 'Name must be 5 characters long!' };
+      });
+    } else {
+      setError(preState => {
+        return { ...preState, name: '' };
+      });
     }
 
-    if (user.password === '') {
-      setPassword('No password');
+    if (password.length < 8) {
+      setError(preState => {
+        return { ...preState, password: 'Password must be 8 characters long!' };
+      });
+    } else {
+      setError(preState => {
+        return { ...preState, password: '' };
+      });
     }
+
+    if (email === '' || name === '' || password === '') {
+      return false
+    }
+    return true;
   }
 
   return (
     <Container>
-      {notifi !== '' && (
-        <CenteredModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-          message={notifi}
-          title={title}
-        ></CenteredModal>
-      )}
-      {error !== '' && (<Alert variant='danger'> {error} </Alert>)}
-      <Form onSubmit={handleSubmit} className='register-form ml-auto mr-auto'>
+      {notifi !== '' &&
+        <div>
+          <CenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            message={notifi}
+            title={title}
+          ></CenteredModal>
+        </div>
+      }
+      <Form className='register-form ml-auto mr-auto'>
         <Form.Group controlId="formRegisterEmail">
           <Form.Label>Your Email</Form.Label>
+          {error.email.length > 0 && <p >{error.email}</p>}
           <Form.Control
+            name="email"
             type="email"
-            onChange={e => setEmail(e.target.value)} />
+            onChange= {(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group controlId="formRegisterName">
           <Form.Label>Your Name</Form.Label>
+          {error.name.length > 0 && <p >{error.name}</p>}
           <Form.Control
+            name="name"
             type="name"
-            onChange={e => setName(e.target.value)}
+            onChange= {(e) => setName(e.target.value)}
           />
         </Form.Group>
 
         <Form.Group controlId="formRegisterPassword">
           <Form.Label>Password</Form.Label>
+          {error.password.length > 0 && <p >{error.password}</p>}
           <Form.Control
+            name="password"
             type="password"
-            onChange={e => setPassword(e.target.value)}
+            onChange= {(e) => setPassword(e.target.value)}
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={handleSubmit}>
           Sign Up
         </Button>
+
       </Form>
     </Container>
   );
